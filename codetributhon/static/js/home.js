3c3e55e40edcfff42513f8e6c1adadc9d3dfb2ef
willkoua <willkoua@gmail.com>
2016-07-18 15:48:24 -0400

    update slide homepage

--- codetributhon/static/js/home.js
+++ codetributhon/static/js/home.js
@@ -5,10 +5,14 @@
 
 $(function(){
 
-    $('.da-slider').backstretch([
+    $('#slider-wrapper').backstretch([
         "../static/img/bg/1.jpeg",
         "../static/img/bg/2.jpg",
         "../static/img/bg/3.jpg"
     ], {duration: 3000, fade: 750});
+
+    $('.carousel').carousel({
+        pause: "null"
+    })
 
 });
