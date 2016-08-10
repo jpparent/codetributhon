72aae155ee6354b5a27cbaf1c8bb10b39357fcd4
willkoua <willkoua@gmail.com>
2016-08-10 13:15:28 -0400

    nouvelle configuration

--- codetributhon/settings.py
+++ codetributhon/settings.py
@@ -84,8 +84,11 @@ WSGI_APPLICATION = 'codetributhon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.codetributhon'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'codetributhondb',
+        'USER': 'root',
+        'PASSWORD': 'ml2_codeTribuThon',
+        'HOST': '',
     }
 }
 
