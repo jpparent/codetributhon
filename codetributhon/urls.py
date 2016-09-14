0fd008244af81156655e79ef88e3a97678b26f7c
Marc Antoine Dumont <dumont.marcantoine13@gmail.com>
2016-09-14 10:32:13 -0400

    Correction of the picture that don't appear in dev mode.
    We had to add a link to the media in dev mode.

--- codetributhon/urls.py
+++ codetributhon/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -27,3 +29,6 @@ urlpatterns = [
     url(r'^events/', include('events.urls', namespace="events")),
 ]
 
+if settings.DEBUG:
+    # static files (images, css, javascript, etc.)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
